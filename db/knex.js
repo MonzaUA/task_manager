2c31ac4bacfb72af934feab8febeeaacb5b1ff1f
Vlad Kvasnetskiy <13kvasnetskiy07@gmail.com>
2026-01-04 20:54:47 +0100

    removed ngrok

--- db/knex.js
+++ db/knex.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile');
 
-const db = knex(knexConfig.development); // беремо dev-конфіг
+const db = knex(knexConfig.development);
 
 module.exports = db;
