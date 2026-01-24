976305a39aa05af73f2023089404ad2fe1dcc181
Vlad Kvasnetskiy <13kvasnetskiy07@gmail.com>
2026-01-24 16:02:35 +0100

    all_is_good

---
+++ migrations/20260124145822_user.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('tasks_knex', (table) => {
+    table.string('user')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('task_knex', (table) => {
+    table.dropColumn('user')
+  })
+};
