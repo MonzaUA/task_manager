3d1ba8c8f219503a46dac2a602bf618b30486a5d
Vlad Kvasnetskiy <13kvasnetskiy07@gmail.com>
2026-01-25 21:26:02 +0100

    docker-setup-DONE

---
+++ dist/graphql/schema.js
@@ -0,0 +1,31 @@
+export const typeDefs = `#graphql
+  type Task {
+    id: ID!
+    title: String!
+    completed: Boolean!
+    user: String!
+  }
+
+  type Query {
+    tasks: [Task!]!
+    task(id: ID!): Task
+  }
+
+  type Mutation {
+    createTask(
+      title: String!
+      user: String!
+      completed: Boolean
+    ): Task!
+
+    updateTask(
+      id: ID!
+      title: String
+      user: String
+      completed: Boolean
+    ): Task!
+
+    deleteTask(id: ID!): Task!
+  }
+`;
+//# sourceMappingURL=schema.js.map
